ebdcda8ccafd072a46a45da770eb91243fe015db
Franziska Horn <cod3licious@gmail.com>
2020-02-24 23:55:54 +0100

    fix version number

--- autofeat/__init__.py
+++ autofeat/__init__.py
@@ -3,6 +3,6 @@
 # License: MIT
 
 name = "autofeat"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 from .autofeat import AutoFeatModel, AutoFeatRegressor, AutoFeatClassifier  # noqa
 from .featsel import FeatureSelector  # noqa
