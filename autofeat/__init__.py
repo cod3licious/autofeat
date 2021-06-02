bd0d6008b5fcc4af3ea9737d64c378b9ae4da968
franzi <cod3licious@gmail.com>
2021-06-02 13:45:27 +0200

    updated version number

--- autofeat/__init__.py
+++ autofeat/__init__.py
@@ -3,7 +3,7 @@
 # License: MIT
 
 name = "autofeat"
-__version__ = "2.0.5"
+__version__ = "2.0.6"
 from .autofeatlight import AutoFeatLight  # noqa
 from .autofeat import AutoFeatModel, AutoFeatRegressor, AutoFeatClassifier  # noqa
 from .featsel import FeatureSelector  # noqa
